6ea0e4c8db8f527f6fd4c6c634fa2f19d535fdb0
cchand-vanc <cmchandler@live.co.uk>
2023-02-01 19:12:16 -0800

    Adds loadTweet function

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,33 +6,10 @@
 
 $(document).ready(() => {
 
-const tweetData = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
+  const tweetDatabase = []
 
-  const renderTweets = function(tweetArray) {
-     
+  const renderTweets = function (tweetArray) {
+
     for (const tweet of tweetArray) {
       const renderedTweet = createTweetElement(tweet);
       $('#tweets-container').append(renderedTweet);
@@ -43,15 +20,11 @@ const tweetData = [
 
     const userInfo = tweet.user;
     const content = tweet.content;
-
-    //Date calculation
-    const currentDate = Math.floor(Date.now());
-    const dateDifference = currentDate - tweet.created_at;
-    const daysAgo = Math.floor(dateDifference / 86400000); //86400000 = num of nanoseconds in a day
+    const daysAgo = timeago.format(tweet.created_at);
 
 
     const $tweet = $(
-    `<article class="tweet">
+      `<article class="tweet">
     
     <header class="tweet-header">
       <span class="profile-info">${userInfo.name}</span>
@@ -63,7 +36,7 @@ const tweetData = [
     </p>
 
     <footer>
-      <span class="date-written">${daysAgo} days ago</span>
+      <span class="date-written">${daysAgo}</span>
       <span class="action-icons">
         <i class="fa-solid fa-flag"></i>
         <i class="fa-solid fa-retweet"></i>
@@ -76,10 +49,10 @@ const tweetData = [
     return $tweet;
   };
 
-  renderTweets(tweetData)
+  renderTweets(tweetDatabase)
 
-//Form Submission
-  $('#tweet-form').submit(function (event) {
+  //Form Submission for new tweets
+  $('#tweet-text').submit(function (event) {
     event.preventDefault();
 
     const tweetData = $('#tweet-text').val();
@@ -90,8 +63,27 @@ const tweetData = [
       url: '/tweets/',
       data: serialData
     })
-    .done(function (res) {
-      console.log('res', res)
+    // .done(function (res) { //mentor suggested using .done
+    //   console.log('res', res)
+    // });
+  });
+
+  const loadTweets = function () {
+    $.ajax({
+      method: 'GET',
+      url: '/tweets/',
+      success: function (response) {
+        console.log(response)
+        return renderTweets(response)
+      },
+      error: function (error) {
+        console.log('Error', error)
+      }
     })
-  })
+  }
+  loadTweets()
+
 });
+
+
+
